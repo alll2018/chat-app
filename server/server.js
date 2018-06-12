1c204b0fbb09ffd92ce8c641ed40b2cf607484a2
Alan Levine <alanllevine@gmail.com>
2018-06-11 21:36:24 -0500

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,16 +13,32 @@ var io = socketIO(server);
 io.on('connection',(socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', 
+    {from: 'Admin',
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+    });
 
+    socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text:  'new user joined chat',
+    createdAt: new Date().getTime()
+});
 
 socket.on('createMessage', (message) => {
     console.log('created new message',message);
      io.emit('newMessage',{
-        from: message.from,
-        text: message.text,
-        createdAt: new Date().getTime()
+         from: message.from,
+         text: message.text,
+         createdAt: new Date().getTime()
+     })
+    // socket.broadcast.emit('newMessage', {
+    //     from:  message.from,
+    //     text:  message.text,
+    //     createdAt: new Date().getTime()
+    //})
     });
-});
+
 
 socket.on('disconnect', () => {
     console.log('User was disconnected');
@@ -36,4 +52,4 @@ const port = process.env.PORT || 3000;
 
 server.listen(port ,() => {
     console.log(`Started on port ${port}`);
-})
+});
